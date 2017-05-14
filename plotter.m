54c2f1a6739c94dcd0a4b3de0dd172c9feccc0e3
Invinyx <invinyx@gmail.com>
2017-05-14 15:27:09 -0400

    OnePlot
    Only Sin Plot

--- plotter.m
+++ plotter.m
@@ -1,21 +1,7 @@
-I=imread('d.jpg');
-pkg load image
-w=size(I,1)
-h=size(I,2)
-J=double(I);
-for i=1:w
-  
-  for j=1:h
-      energy=J(i,j,1)^2+J(i,j,2)^2+J(i,j,3)^2;
-      red=double(J(i,j,1));
-      energy=sqrt(double(energy));
-      if( red/energy>.7)
-        I(i,j,1)=204;
-        I(i,j,2)=201;
-        I(i,j,3)=255;
-      endif
-    endfor
- endfor
- imshow(I)
- 
- imwrite(I,'f2.bmp');
+%% This code plots a graphics_toolkit
+
+t=(0:0.01:2);
+
+y1=sin(t);
+
+plot(t,y1,'r');
